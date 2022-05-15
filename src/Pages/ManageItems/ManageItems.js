75eed8635d2230c7366196d591e752e899427441
Arif11819 <93979453+Arif11819@users.noreply.github.com>
2022-05-15 19:17:16 +0600

    added file after finished delete items

--- src/Pages/ManageItems/ManageItems.js
+++ src/Pages/ManageItems/ManageItems.js
@@ -1,34 +1,73 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { Table } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
-import InventoryItem from '../Home/InventoryItem/InventoryItem';
+import useItems from '../../hooks/useItems';
+import './ManageItems.css';
 
 const ManageItems = () => {
-    const [items, setItems] = useState([]);
+    const [items, setItems] = useItems();
     const navigate = useNavigate();
-    useEffect(() => {
-        fetch('http://localhost:5000/items')
-            .then(res => res.json())
-            .then(data => setItems(data))
-    }, [])
 
     const handleAddNewInventory = () => {
         navigate('/addInventory');
     }
 
-    return (
-        <div>
-            <h1 className='text-center text-danger mt-4 mx-auto'>All Inventory Items</h1>
-            <div>
-                {
-                    items.map(item => <InventoryItem
-                        key={item._id}
-                        item={item}
-                    >
+    const handleNavigateToItems = (id) => {
+        navigate(`/items/${id}`);
+    }
 
-                    </InventoryItem>)
-                }
-                <button onClick={handleAddNewInventory} className='btn btn-dark manage-inventory-btn'>Add New Item</button>
-            </div>
+    const handleDelete = id => {
+        const proceed = window.confirm('Are you sure delete this?');
+        if (proceed) {
+            const url = `http://localhost:5000/items/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    const remainingItems = items.filter(item => item._id !== id);
+                    setItems(remainingItems);
+                })
+        }
+    }
+    return (
+        <div className='inventory-items'>
+            <h1 className='text-center text-danger mb-4'>All Inventory Items</h1>
+            {
+                items.map(item => <div key={item._id}>
+                    <Table striped bordered hover variant="dark">
+                        <thead>
+                            <tr>
+                                <th className='text-center'>Id</th>
+                                <th className='text-center'>Name</th>
+                                <th className='text-center'>Picture</th>
+                                <th className='text-center'>Price</th>
+                                <th className='text-center'>Description</th>
+                                <th className='text-center'>Quantity</th>
+                                <th className='text-center'>Supplier</th>
+                                <th className='text-center'>Update</th>
+                                <th className='text-center'>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td className='text-center'>{item._id}</td>
+                                <td className='text-center'>{item.name}</td>
+                                <td className='text-center'><img className='picture' src={item.picture} alt="" /></td>
+                                <td className='text-center'>${item.price}</td>
+                                <td className='text-center'>{item.description}</td>
+                                <td className='text-center'>{item.quantity}</td>
+                                <td className='text-center'>{item.supplier}</td>
+                                <td className='text-center'><button onClick={() => handleNavigateToItems(item._id)} className='update-btn'>Update</button></td>
+                                <td className='text-center mx-auto'><button onClick={() => handleDelete(item._id)} className='delete-btn'>Detete</button></td>
+                            </tr>
+                        </tbody>
+                    </Table>
+
+                </div>)
+            }
+            <button onClick={handleAddNewInventory} className='btn btn-dark manage-inventory-btn'>Add New Item</button>
         </div>
     );
 };
